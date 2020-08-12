868b4d0c5ad470db057b43aaf4fbea8b4ef4dd67
namanshahnemei <69518068+namanshahnemei@users.noreply.github.com>
2020-08-13 00:47:58 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,19 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
 
-echo'Hello world';
-echo'Hi ';
-?>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <h1> <b> MY WEBSITE </b> </h1>
+</head>
+
+<body>
+    <?php
+
+    echo 'Hello world';
+    echo 'Hi ';
+    ?>
+</body>
+
+</html>

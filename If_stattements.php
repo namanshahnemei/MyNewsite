8726f428d6279ab591d4637d8f49d66a9f36d4a1
namanshahnemei <69518068+namanshahnemei@users.noreply.github.com>
2020-08-13 01:36:40 +0530

    Update If_stattements.php

--- If_stattements.php
+++ If_stattements.php
@@ -1,40 +1,8 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 <?php
-$a=10;
-if($a>70)
-{
-    echo"a: $a is greater than 70";
+$a = 10;
+if ($a > 70) {
+    echo "a: $a is greater than 70";
+} else {
+    echo "a: $a is less than 70";
 }
-else
-{
-    echo"a: $a is less than 70";
-}
-
-=======
-<?php
-$a=10;
-if($a>70)
-{
-    echo"a: $a is greater than 70";
-}
-else
-{
-    echo"a: $a is less than 70";
-}
-
->>>>>>> ae2e97707bb9f80a7e2a108dc7ba6f9253983982
-=======
-<?php
-$a=10;
-if($a>70)
-{
-    echo"a: $a is greater than 70";
-}
-else
-{
-    echo"a: $a is less than 70";
-}
-
->>>>>>> ae2e97707bb9f80a7e2a108dc7ba6f9253983982
 ?>
